98ab984d3965ce463d4536ccc95facbd5e219107
hyunwoopark <migusd>
2022-07-27 12:37:51 +0900

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+
+var excel_data;
+Swal.fire({
+    title: '파일을 선택해주세요',
+    input: 'file',
+    showCancelButton: true,
+    confirmButtonText: '네',
+    cancelButtonText: '아니요',
+    showLoaderOnConfirm: true,
+
+    preConfirm: function () {
+        return new Promise((resolve, reject) => {
+            setTimeout(
+                () => {
+                    var reader = new FileReader();
+                    var file = document.querySelector('.swal2-file').files[0];
+                    var wb;
+                    reader.onload = function () {
+                        var fileData = reader.result;
+                        wb = XLSX.read(fileData, {type: 'binary'});
+                        console.log(wb);
+                        excel_data = sheetToJson(wb);
+                        resolve();
+                    }
+                    reader.readAsBinaryString(file);
+                }, 0
+            );
+        });
+    }
+}).then(()=>{
+    function onScanSuccess(decodedText, decodedResult) {
+        console.log(`Code scanned = ${decodedText}`, decodedResult);
+        alert(decodedResult);
+    }
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+        "qr-reader", { fps: 10, qrbox: 250 });
+    html5QrcodeScanner.render(onScanSuccess);
+})
+
+function sheetToJson(wb) {
+    let result = new Array();
+    wb.SheetNames.forEach(sheetName => {
+        let rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+        result.push(rowObj);
+    })
+    return result;
+}
